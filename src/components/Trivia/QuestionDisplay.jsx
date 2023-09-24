a37ffc60c42c2178f8e5c3d377915c61ef9944b4
Gaspare Tortora <g.tortora2@gmail.com>
2023-09-24 00:17:40 -0400

    Changed input radio buttons styling

--- src/components/Trivia/QuestionDisplay.jsx
+++ src/components/Trivia/QuestionDisplay.jsx
@@ -11,24 +11,26 @@ function QuestionDisplay({ question, currentQuestionIndex, userAnswers, onAnswer
       <p>{decodedQuestion}</p> {/* use the decoded question */}
       {/* render answer options */}
       {question.incorrect_answers.map((answer, i) => (
-        <label key={i}>
+        <label key={i} htmlFor={`q${currentQuestionIndex}_incorrect_${i}`}>
           <input
             type="radio"
             name={`q${currentQuestionIndex}`}
+            id={`q${currentQuestionIndex}_incorrect_${i}`}
             value={answer}
             onChange={() => onAnswerSelect(answer)}
           />{' '}
-          {he.decode(answer)} {/* decode answer options as well */}
+          <p className='answer'>{he.decode(answer)}</p> {/* decode answer options as well */}
         </label>
       ))}
-      <label>
+      <label htmlFor={`q${currentQuestionIndex}_correct`}>
         <input
           type="radio"
           name={`q${currentQuestionIndex}`}
+          id={`q${currentQuestionIndex}_correct`}
           value={question.correct_answer}
           onChange={() => onAnswerSelect(question.correct_answer)}
         />{' '}
-        {he.decode(question.correct_answer)} {/* decode correct answer */}
+        <p className='answer'>{he.decode(question.correct_answer)}</p> {/* decode correct answer */}
       </label>
 
       {/* place the Next Question button underneath */}
