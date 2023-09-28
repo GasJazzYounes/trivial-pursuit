399e03446de5f5faebb79a8a2fdbd5d864d41a86
Younes <younes.bns@outlook.com>
2023-09-28 19:26:33 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import './app.css';
+import "./app.css";
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
-import Homepage from './components/Homepage/Homepage';
-import Instructions from './components/Instructions/Instructions'; 
-import Leaderboard from './components/Leaderboard/Leaderboard'; 
-import Trivia from './components/Trivia/Trivia';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Homepage from "./components/Homepage/Homepage";
+import Instructions from "./components/Instructions/Instructions";
+import Leaderboard from "./components/Leaderboard/Leaderboard";
+import Trivia from "./components/Trivia/Trivia";
 
 function App() {
   return (
@@ -16,7 +16,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/instructions" element={<Instructions />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/trivia" element={<Trivia />} /> {/* Use the Trivia component */}
+          <Route path="/trivia" element={<Trivia />} />{" "}
+          {/* Use the Trivia component */}
         </Routes>
       </div>
     </Router>
