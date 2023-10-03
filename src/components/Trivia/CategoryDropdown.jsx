4266fbef756726b2ab018d0222bb83091824ed85
Gaspare Tortora <g.tortora2@gmail.com>
2023-10-03 12:33:06 -0400

    made form mandatory so that you can't play without selecting options

--- src/components/Trivia/CategoryDropdown.jsx
+++ src/components/Trivia/CategoryDropdown.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function CategoryDropdown({ categories, selectedCategory, onCategoryChange }) {
   return (
     <>
-      <select value={selectedCategory} onChange={(e) => onCategoryChange(e.target.value)}>
+      <select value={selectedCategory} onChange={(e) => onCategoryChange(e.target.value)} required>
         <option value="" selected disabled>Select a category</option>
         {categories.map(category => (
           <option key={category.id} value={category.id}>
